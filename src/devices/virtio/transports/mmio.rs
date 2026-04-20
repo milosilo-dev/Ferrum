9a8c06b3da36d5eeb2be4837dcb16150055d06ee
milosilo-dev <lazerache@gmail.com>
2026-04-20 16:58:27 +0100

    virtio

--- src/devices/virtio/transports/mmio.rs
+++ src/devices/virtio/transports/mmio.rs
@@ -1,22 +1,43 @@
-use crate::device_maps::mmio::MMIODevice;
+use crate::{device_maps::mmio::MMIODevice, devices::virtio::virtio::{VirtioDevice, VirtioQueue}};
+
+const MAGIC_NUMBER: u32 = 0x74726976;
+const VERSION: u32 = 0x2;
+const VENDOR_ID: u32 = 0x56484B53;
 
 pub struct MMIOTransport {
-    queue_ready: u8,
-    interrupt_status: u64,
+    device: Box<dyn VirtioDevice + Sync + Send>,
+    queue: Vec<VirtioQueue>,
+
+    queue_sel: usize,
+    status: u32,
+    interrupt_status: u32,
+}
+
+impl MMIOTransport {
+    pub fn new(device: Box<dyn VirtioDevice + Sync + Send>) -> Self{
+        Self {
+            device,
+            queue: vec![VirtioQueue::new()],
+            queue_sel: 0,
+            status: 0,
+            interrupt_status: 0,
+        }
+    }
 }
 
 impl MMIODevice for MMIOTransport {
     fn read(&mut self, addr: u64, length: usize) -> Vec<u8> {
         let value = (match addr {
-            0x000 => 0x74726976,                // Magic number
-            0x004 => 0x2,                       // Version
-            0x008 => 0x2,                       // Device block
-            0x00C => 0x56484B53,                // Vendor ID
-            0x010 => 0x0,                       // Device Fetures
-            0x034 => 64,                        // Queue size
-            0x044 => self.queue_ready as u64,   // Queue ready
-            0x060 => self.interrupt_status,     // IRQ Status
-            0x070 => 0,                         // Status
+            0x000 => MAGIC_NUMBER,
+            0x004 => VERSION,
+            0x008 => self.device.virtio_type(),
+            0x00C => VENDOR_ID,
+            0x010 => self.device.features(),
+            0x034 => self.queue_sel as u32,
+            0x038 => self.queue[self.queue_sel].size as u32,
+            0x044 => self.queue[self.queue_sel].ready as u32,
+            0x070 => self.status,
+            0x060 => self.interrupt_status,
             _ => 0,
         } as u64).to_le_bytes();
         value[..length].to_vec()
--- src/devices/virtio/virtio.rs
+++ src/devices/virtio/virtio.rs
@@ -1,3 +1,25 @@
-pub struct VirtioDevice {
-    
+pub trait VirtioDevice {
+    fn virtio_type(&self) -> u32;
+    fn features(&self) -> u32;
+}
+
+pub struct VirtioQueue {
+    pub size: u16,
+    pub ready: bool,
+
+    pub desc_addr: u64,
+    pub avail_addr: u64,
+    pub used_addr: u64,
+}
+
+impl VirtioQueue {
+    pub fn new() -> Self{
+        Self{
+            size: 0,
+            ready: false,
+            desc_addr: 0,
+            avail_addr: 0,
+            used_addr: 0,
+        }
+    }
 }
