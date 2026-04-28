ab49f19ebaaf672464124d0499b340abcb2ac570
milosilo-dev <lazerache@gmail.com>
2026-04-28 18:47:20 +0100

    Moved mbr read into long mode

--- guest/firmware/main64.c
+++ guest/firmware/main64.c
@@ -1,7 +1,15 @@
 #include "serial.h"
+#include "blk.c"
 
 void c_main_64(void) {
     serial_puts("Long mode!\n");
+
+    uint8_t sector[512];
+    uint32_t status = virtio_blk_read(0, 512, sector);
+    serial_puts("status: "); serial_putx(status); serial_puts("\n");
+    serial_puts("MBR sig: ");
+    serial_putx(sector[510]); serial_putc(' ');
+    serial_putx(sector[511]); serial_puts("\n");
 
     // spin forever
     while (1) {
